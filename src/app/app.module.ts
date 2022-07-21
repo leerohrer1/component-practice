e0cf95c42c53b5e3deb3d5a88b67e5a27ce861ce
leerohrer1 <loginlee3@gmail.com>
2022-07-20 19:38:49 -0500

    getting practice pushed to github

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TaskListComponent } from './task-list/task-list.component';
+import { HeaderComponent } from './header/header.component';
+import { InsideTaskListComponent } from './task-list/inside-task-list/inside-task-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TaskListComponent,
+    HeaderComponent,
+    InsideTaskListComponent
   ],
   imports: [
     BrowserModule
